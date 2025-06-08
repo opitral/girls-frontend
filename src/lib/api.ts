577b3e070c3928a05f10bbe4072a6a83b18561fe
ghtx280 <An280502@gmail.com>
2025-06-09 00:42:38 +0300

    all loging fetch

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,10 @@
 const BASE = 'http://195.189.227.62:8080';
 
 export async function getJson<T>(path: string, params?: string): Promise<T> {
-	const response = await fetch(`${BASE}/${path}?${params}`);
+	const url = `${BASE}/${path}?${params}`;
+	console.log('start getting', url);
+	const response = await fetch(url);
+	console.log('end getting', url);
 	const data = await response.json();
 	return data;
 }
